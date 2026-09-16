c54384761620de8e2e301cdf2517d0a9ce3406d1
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-16 22:36:14 +0530

    BS BASIC

--- BS_basic.cpp
+++ BS_basic.cpp
@@ -1,1 +1,29 @@
 //$ BINARY SEARCH BASIC
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int fnx1(int arr[],int n,int target) {
+    int low = 0,high = n-1,mid;
+    while(low <= high){
+        mid = (low+high)/2;
+
+        if(arr[mid] == target)
+        return mid;
+
+        else if(target > arr[mid])
+        low = mid+1;
+        
+        else high = mid-1;
+    }
+    return -1;
+}
+
+int main(){
+    int target = 12;
+    int arr[]= {3,4,6,7,9,12,16,17};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    fnx1(arr,n,target);
+    return 0;
+}
