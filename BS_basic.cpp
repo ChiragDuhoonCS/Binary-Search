f1a73b662081b44be0fce10acf5d40cb964f437e
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-16 22:54:05 +0530

    recursive

--- BS_basic.cpp
+++ BS_basic.cpp
@@ -3,6 +3,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//!ITERATION
+
 int fnx1(int arr[],int n,int target) {
     int low = 0,high = n-1,mid;
     while(low <= high){
@@ -19,7 +21,24 @@ int fnx1(int arr[],int n,int target) {
     return -1;
 }
 
-int main(){
+//! BY RECUSRION
+int fnx1Recursive(int arr[], int target, int low, int high) {
+    if (low > high)
+        return -1;
+
+    int mid = low + (high - low) / 2;
+
+        if(arr[mid] == target)
+        return mid;
+
+        else if(target > arr[mid])
+        return fnx1Recursive(arr, target, mid + 1, high);
+        
+        else
+        return fnx1Recursive(arr, target, low, mid - 1);
+}
+
+/*int main(){
     int target = 12;
     int arr[]= {3,4,6,7,9,12,16,17};
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -31,4 +50,5 @@ int main(){
 
 
     return 0;
-}
+}*/
+
