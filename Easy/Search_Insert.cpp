f3ff0d581aa241d9fa387a33c05b98ce8c711d92
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-17 09:58:31 +0530

    [VECTOR] search insert position

--- Easy/Search_Insert.cpp
+++ Easy/Search_Insert.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int insert(int arr[], int n, int target) {
+/*int insert(int arr[], int n, int target) {*/
+
+int findInsertPosition(vector<int>& arr, int target) {
     int low = 0;
-    int high = n - 1;
-    int ans = n; // Default answer if target is greater than all elements
+    int high = arr.size() - 1;
+    int ans = arr.size(); // Default answer if target is greater than all elements
 
     while (low <= high) {
         int mid = low + (high - low) / 2;
@@ -22,18 +24,36 @@ int insert(int arr[], int n, int target) {
     return ans;
 }
 
-int main(){
+/*int main(){
     int target = 5;
     int arr[]= {3,4,6,7,9,12,16,17};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     int ans = insert(arr,n,target);
+    cout << "The target is at index: " << arr << endl;
 
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d , ",arr[i]);
+
+    return 0;
+}*/
+
+//! we are using vectors to insert stuff in it
+int main() {
+    int target = 5;
+    vector<int> arr = {3, 4, 6, 7, 9, 12, 16, 17};
+
+    // Find the correct index using your search logic
+    int ans = findInsertPosition(arr, target);
+
+    // Insert the target into the vector at the found index
+    arr.insert(arr.begin() + ans, target);
+
+    cout << "Updated Array elements: ";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
     }
-    
+    cout << endl;
+
+    cout << "Target " << target << " was inserted at index: " << ans << endl;
 
     return 0;
 }
