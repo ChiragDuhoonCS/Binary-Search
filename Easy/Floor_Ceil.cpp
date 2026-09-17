9f0353b0793c096901805c7c452f50dc0982de67
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-17 10:30:41 +0530

    Ceil value of sorted array

--- Easy/Floor_Ceil.cpp
+++ Easy/Floor_Ceil.cpp
@@ -5,10 +5,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#include <bits/stdc++.h>
-using namespace std;
 
-// Floor = largest element <= target
+//! Floor = largest element <= target
 int floorValue(int arr[], int target, int n)
 {
     int low = 0;
@@ -38,6 +36,36 @@ int floorValue(int arr[], int target, int n)
     return ans;
 }
 
+
+int ceilValue(int arr[], int target, int n)
+{
+    int low = 0;
+    int high = n - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2; //
+
+        if (arr[mid] >= target)
+        {
+            // arr[mid] can be the floor
+            ans = arr[mid];
+
+            high = mid - 1; //! here its different
+
+        }
+        else
+        {
+            low = mid + 1; //! here    they just swap
+
+        }
+    }
+
+    return ans;
+}
+
+
 int main()
 {
     int arr[] = {3, 4, 6, 7, 9, 12, 16, 17};
@@ -45,7 +73,7 @@ int main()
 
     int target = 5;
 
-    int ans = floorValue(arr, target, n);
+    int ans = ceilValue(arr, target, n);
 
     cout << "Target: " << target << endl;
     cout << "Floor value: " << ans << endl; //@ floor value not index
