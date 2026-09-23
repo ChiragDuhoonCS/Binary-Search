64d246402d0935318270ab96f2579fa3bcdc5246
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-24 00:42:43 +0530

    smallest integer

---
+++ Easy/smallest_divisor.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void brute(int n,int arr[], int d){
+    for (int i = arr[0]; i <= arr[n-1]; i++)
+    {
+        
+    }
+    
+}
+
+int main() {
+    int arr[] = {1,2,5,9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int d = 6;
+
+    printf("Your peak value is at: %d ",arr);
+
+    return 0;
+}
