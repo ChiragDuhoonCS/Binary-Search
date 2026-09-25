f0ab37f2fa8275c43e0a2513def280ebd1843171
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-25 13:45:07 +0530

    optimise solution of finding

--- Easy/searchI.cpp
+++ Easy/searchI.cpp
@@ -12,15 +12,37 @@ int brute(int n,int arr[],int target) {
     
 }
 
+int optimise(int n,int arr[],int target) {
+    int low = 0;
+    int high = n-1;
+        while (low <= high) {
+        int mid = (low + high )/2;
+
+        if (arr[mid] == target ) return mid;
+        else if (arr[low] <= arr[mid]) {
+            if (arr[low] <= target && target < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else {
+            if (arr[mid] < target && target <= arr[high])
+                low = mid + 1;
+            
+        }
+    }
+    return -1;
+}
+
 int main() {
     int arr[] = {7,8,9,1,2,3,4,5,6};
     int n = sizeof(arr) / sizeof(arr[0]);
     int target = 1;
 
-    int ans = brute(n,arr,target);
+    int ans = optimise(n,arr,target);
 
 
-    printf("Search in rotated array position: %d ",arr[ans]);
+    printf("Search in rotated array position: %d ",ans);
 
     return 0;
 }
