eab392d4868bd2443bbad152240b0930292763c1
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-25 21:27:22 +0530

    agressive cows [error]

---
+++ Easy/agressive_cow.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool canplace() {
+    if();
+}
+
+
+void brute(int n,int arr[],int cows){
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+    }
+    
+}
+
+int main() {
+    int arr[] = {0,3,4,7,9,10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int cows = 4;
+
+    int ans;
+}
